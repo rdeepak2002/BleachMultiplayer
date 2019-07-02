bafa893422ddc8dd2d9e704bfd4e57d4f2f6d0cc
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-07-02 10:35:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+
+// sets port 8080 to default or unless otherwise specified in the environment
+app.set('port', process.env.PORT || 8080);
+
+app.use(express.static(__dirname))
+
+app.get('/', function(req, res){
+	res.sendFile( __dirname + "/game.html" );
+});
+
+app.listen(app.get('port'));
+
+(function() {
+  var io;
+  io = require('socket.io').listen(4000);
+  io.sockets.on('connection', function(socket) {
+    socket.on('drawClick', function(data) {
+      socket.broadcast.emit('draw', {
+        x: data.x,
+        y: data.y,
+        type: data.type
+      });
+    });
+  });
+}).call(this);
