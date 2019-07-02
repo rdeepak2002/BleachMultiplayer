765f5f99d96260e5bc72c8831ac19f38041ecd74
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-07-02 11:10:06 +0530

    added to npm save

--- server.js
+++ server.js
@@ -10,18 +10,18 @@ app.get('/', function(req, res){
 	res.sendFile( __dirname + "/game.html" );
 });
 
-// app.listen(app.get('port'));
+app.listen(app.get('port'));
 
-// (function() {
-//   var io;
-//   io = require('socket.io').listen(4000);
-//   io.sockets.on('connection', function(socket) {
-//     socket.on('drawClick', function(data) {
-//       socket.broadcast.emit('draw', {
-//         x: data.x,
-//         y: data.y,
-//         type: data.type
-//       });
-//     });
-//   });
-// }).call(this);
+(function() {
+  var io;
+  io = require('socket.io').listen(4000);
+  io.sockets.on('connection', function(socket) {
+    socket.on('drawClick', function(data) {
+      socket.broadcast.emit('draw', {
+        x: data.x,
+        y: data.y,
+        type: data.type
+      });
+    });
+  });
+}).call(this);
