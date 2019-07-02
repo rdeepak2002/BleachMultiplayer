dc0974ecbae321a73f25eda4f133be0801bc26f3
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-07-02 12:08:31 +0530

    cleaning up code

--- script.js
+++ script.js
@@ -14,8 +14,6 @@
     App.ctx.strokeStyle = "#ECD018";
     App.ctx.lineWidth = 5;
     App.ctx.lineCap = "round";
-    //App.socket = io.connect('http://bleachonline.herokuapp.com:4000');
-    //App.socket = io.connect('http://bleachonline.herokuapp.com:4000');
     App.socket = io();
     App.socket.on('log', function(data) {
       console.log(data);
