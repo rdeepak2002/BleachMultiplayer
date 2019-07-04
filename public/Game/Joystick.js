e45e7347f031cd089eda754610388120abe17734
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-07-04 18:07:00 +0530

    added mobile device check

--- public/Game/Joystick.js
+++ public/Game/Joystick.js
@@ -14,25 +14,48 @@ var sampleJoystick = {
 var joystick;
 var position;
 
-joystick = nipplejs.create(sampleJoystick);
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
 
-joystick.on('start', function(evt, data) {
-  position = data;
-}).on('end', function(evt, data) {
-  position = data;
-  joyStickDirection = "none";
-}).on('move', function(evt, data) {
-  position = data;
-  if(position.direction == undefined) {
+if( isMobile.any() ) {
+  joystick = nipplejs.create(sampleJoystick);
+
+  joystick.on('start', function(evt, data) {
+    position = data;
+  }).on('end', function(evt, data) {
+    position = data;
     joyStickDirection = "none";
-  }
-  else {
-    joyStickDirection = position.direction.angle; 
-  }
-}).on('dir:up plain:up dir:left plain:left dir:down' +
-  'plain:down dir:right plain:right',
-  function(evt, data) {
-  //position=data;
-}).on('pressure', function(evt, data) {
-  position=data;
-});
+  }).on('move', function(evt, data) {
+    position = data;
+    if(position.direction == undefined) {
+      joyStickDirection = "none";
+    }
+    else {
+      joyStickDirection = position.direction.angle; 
+    }
+  }).on('dir:up plain:up dir:left plain:left dir:down' +
+    'plain:down dir:right plain:right',
+    function(evt, data) {
+    //position=data;
+  }).on('pressure', function(evt, data) {
+    position=data;
+  });
+}
