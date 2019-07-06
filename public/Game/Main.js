81209885cae53e6230728f2529d7e89bda28c0a7
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-07-06 16:56:25 +0530

    added loading for images

--- public/Game/Main.js
+++ public/Game/Main.js
@@ -57,7 +57,8 @@ $(function() {
         var precent = Math.round(100*(1.0*numLoaded/size));
         App.ctx.font = "5rem Arial";
         App.ctx.fillStyle = "rgb(255, 255, 255)";
-        App.ctx.fillText("LOADING: " + precent, 10, 70);
+        percent = numLoaded + "/" + size;
+        App.ctx.fillText("LOADING: " + precent + "%", 10, 70);
         console.log("LOADING: " + precent);
       }
 
