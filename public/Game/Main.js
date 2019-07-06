8ba7f5153df5ae9a52d1bd6d4a9ca43f691570d2
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-07-06 17:00:11 +0530

    added loading for images

--- public/Game/Main.js
+++ public/Game/Main.js
@@ -54,11 +54,11 @@ $(function() {
         App.drawGui(curPlayer);
       }
       else {
-        //var precent = Math.round(100*(1.0*numLoaded/size));
+        //var percent = Math.round(100*(1.0*numLoaded/size));
         App.ctx.font = "5rem Arial";
         App.ctx.fillStyle = "rgb(255, 255, 255)";
         var percent = numLoaded + "/" + size;
-        App.ctx.fillText("LOADING: " + precent + "%", 10, 70);
+        App.ctx.fillText("LOADING: " + percent + "%", 10, 70);
         console.log("LOADING: " + precent);
       }
 
